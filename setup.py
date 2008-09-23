487e54679cb66fd6a7d61042d0ad04f3ccc1d2af
jason kirtland <jek@discorporate.us>
2008-09-22 21:10:30 -0700

    A couple doc updates. Excursion into sphinx templates started, then backed out and moved to a new pickle builder in flatland-www.

--- setup.py
+++ setup.py
@@ -1,8 +1,48 @@
-#!/bin/env python
+"""
+flatland
+~~~~~~~~
 
-from setuptools import setup, find_packages
+Flatland manages the mapping between structured Python application
+data and the flat key/value namespace of forms.  Features include:
+
+  - Declarative form specification, independent of front-end
+    representation
+
+  - Conversion to and from native Python types and Unicode
+
+  - Structured data: lists, dicts, lists of dicts, etc.
+
+  - JavaScript-safe name flattening by default
+
+  - Schema-driven, directed expansion of incoming key/value pairs-
+    only expected request data is examined and parsed
+
+  - Compound fields
+
+  - Validation
+
+  - Strong value defaulting, pre-populating, re-populating and
+    round-tripping
+
+  - Namespacing: easily manage multiple forms in a single page or
+    submission
+
+  - Works on form and JSON data
+
+Flatland data is not anonymous. Values retain knowledge of their
+location in the form hierarchy and their validation status.  Flatland
+tries to provide both pythonic data access in application code and
+also simple and foolproof data access in Web templates.
+
+The hg `flatland tip`_ can be installed via ``easy_install
+flatland==dev``.
+
+.. _flatland tip: http://bitbucket.org/jek/flatland/get/tip.zip#egg=flatland-dev
+
+"""
 
 import sys
+from setuptools import setup, find_packages
 
 # Restrict to Python 2.5, for now.  Provide compatibilty for 2.4 later.
 if sys.version_info < (2, 5):
@@ -24,34 +64,7 @@ setup(name="flatland",
       description='HTML form management and validation',
       keywords='wsgi web http webapps form forms validation roundtrip',
 
-      long_description="""
-Flatland manages the mapping between structured Python application data and
-the flat key/value namespace of forms.  Features include:
-
-  - Declarative form specification
-  - Conversion to and from native Python types and Unicode
-  - Structured data: lists, dicts, lists of dicts, etc.
-  - JavaScript-safe name flattening by default
-  - Schema-driven, directed expansion of incoming key/value pairs-
-    only expected request data is examined and parsed
-  - Compound fields
-  - Validation
-  - Strong value defaulting, pre-populating re-populating and roundtripping
-  - Namespacing: easily manage multiple forms in a single page
-  - Works on form and JSON data
-
-Flatland data is not anonymous- values retain knowledge of their location in
-the form hierarchy and validation status.  Flatland tries to provide both
-pythonic data access in application code and also simple and foolproof data
-access in Web templates.
-
-The hg `flatland tip`_ can be installed via ``easy_install
-flatland==dev``.
-
-.. _flatland tip: http://bitbucket.org/jek/flatland/get/tip.zip#egg=flatland-dev
-
-      """,
-
+      long_description=__doc__,
       license='MIT License',
       url='http://discorporate.us/jek/projects/flatland/',
       classifiers=['Development Status :: 3 - Alpha',
@@ -62,5 +75,5 @@ flatland==dev``.
                    'Operating System :: OS Independent',
                    'Programming Language :: Python',
                    'Topic :: Internet :: WWW/HTTP :: WSGI',
-                   'Topic :: Software Development :: Libraries']
+                   'Topic :: Software Development :: Libraries'],
 )
