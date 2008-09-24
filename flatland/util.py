5e652c80b3d0e47cf1d765db83d4733ac4d53484
jason kirtland <jek@discorporate.us>
2008-09-23 23:52:43 -0700

    It's better for symbols to repr as their bare names.

--- flatland/util.py
+++ flatland/util.py
@@ -262,7 +262,7 @@ class _symbol(object):
     def __reduce__(self):
         return symbol, (self.name,)
     def __repr__(self):
-        return "<symbol '%s>" % self.name
+        return self.name
 _symbol.__name__ = 'symbol'
 
 # derived from SQLAlchemy (http://www.sqlalchemy.org/); MIT License
