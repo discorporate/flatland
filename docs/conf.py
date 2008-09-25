8258589a02e25f7c28e630e746ff122d6bfbe818
jason kirtland <jek@discorporate.us>
2008-09-24 23:25:28 -0700

    Missed the _directories when unwinding sphinx customizations.

--- docs/conf.py
+++ docs/conf.py
@@ -28,7 +28,7 @@ sys.path.append(path.abspath(path.dirname(__file__) + "../../"))
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest']
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['templates']
+templates_path = ['_templates']
 
 # The suffix of source filenames.
 source_suffix = '.txt'
@@ -108,7 +108,7 @@ pygments_style = 'sphinx'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['static']
+html_static_path = ['_static']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
