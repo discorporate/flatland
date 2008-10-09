aff7a4f17849e4c3c0a121bb97236b810f046f72
jason kirtland <jek@discorporate.us>
2008-10-09 12:24:14 -0700

    Modernized compound fields and added coverage.

--- flatland/schema/compound.py
+++ flatland/schema/compound.py
@@ -4,66 +4,110 @@ from . import scalars, containers
 from flatland import exc
 
 
-class _CompoundElement(scalars._ScalarElement, containers._DictElement):
-    def _get_u(self):
-        u, value = self.compose()
-        return u
+class _CompoundElement(containers._DictElement, scalars._ScalarElement):
 
-    def _set_u(self, value):
-        self.explode(value)
+    def u(self):
+        uni, value = self.schema.compose(self)
+        return uni
 
-    u = property(_get_u, _set_u)
+    def set_u(self, value):
+        self.schema.explode(self, value)
 
-    def _get_value(self):
-        u, value = self.schema.compose(self)
+    u = property(u, set_u)
+    del set_u
+
+    def value(self):
+        uni, value = self.schema.compose(self)
         return value
 
-    def _set_value(self, value):
-        self.schema.explode(value)
+    def set_value(self, value):
+        self.schema.explode(self, value)
 
-    value = property(_get_value, _set_value)
-
-    def compose(self):
-        return self.schema.compose(self)
-
-    def explode(self, value):
-        return self.schema.explode(self, value)
+    value = property(value, set_value)
+    del set_value
 
     def set(self, value):
         self.schema.explode(self, value)
 
-    def _el(self, path):
-        if path:
-            return self[path[0]]._el(path[1:])
-        elif not path:
-            return self
-        raise KeyError()
-
     def _set_flat(self, pairs, sep):
         containers._DictElement._set_flat(self, pairs, sep)
 
+    def __repr__(self):
+        try:
+            return scalars._ScalarElement.__repr__(self)
+        except Exception, exc:
+            return '<%s %r; value raised %s>' % (
+                type(self.schema).__name__, self.name, type(exc).__name__)
 
-class Compound(scalars.Scalar, containers.Mapping):
+
+class Compound(containers.Mapping, scalars.Scalar):
+    """A mapping container that acts as a scalar proxy for its children.
+
+    Compound fields are dictionary-like fields that can assemble a
+    :attr:`.u` and :attr:`.value` from their children, and can apply a
+    structures value passed to a :meth:`.set` to its children.
+
+    A simple example is a logical calendar date field composed of 3
+    separate component fields, year, month and day.  The Compound can
+    wrap the 3 parts up into a single field that handles
+    :class:`datetime.date` values.
+
+    :class:`Compound` is an abstract class.  Subclasses must implement
+    :meth:`compose` and :meth:`explode`.
+
+    Composites run validation after their children.
+
+    """
+
     element_type = _CompoundElement
 
-    def __init__(self, name, *specs, **kw):
+    def __init__(self, name, *fields, **kw):
         super(Compound, self).__init__(name, **kw)
 
-        self.fields = {}
-        for spec in specs:
-            if hasattr(spec, 'name') and spec.name:
-                self.fields[spec.name] = spec
-        self.specs = specs
+        if any(not field.name for field in fields):
+            raise TypeError("Child fields of %s %r must be named." %
+                            type(self).__name__, name)
+
+        self.fields = dict((field.name, field) for field in fields)
 
     def compose(self, element):
+        """Return a unicode, native tuple built from children's state.
+
+        :param element: a :class:`_CompoundElement`, a dict-like
+            element type.
+        :returns: a 2-tuple of unicode representation, native value.
+           These correspond to the :meth:`Scalar.serialize_element`
+           and :meth:`Scalar.adapt_element` methods of :class:`Scalar`
+           objects.
+
+        For example, a compound date field may return a '-' delimited
+        string of year, month and day digits and a
+        :class:`datetime.date`.
+
+        """
         raise NotImplementedError()
 
     def explode(self, element, value):
+        """Given a compound value, assign values to children.
+
+        :param element: a :class:`_CompoundElement`, a dict-like
+            element type.
+        :param value: a value to be adapted and exploded
+
+        For example, a compound date field may read attributes from a
+        :class:`datetime.date` value and :meth:`.set()` them on child
+        fields.
+
+        The decision to perform type checking on *value* is completely
+        up to you and you may find you want different rules for
+        different compound types.
+
+        """
         raise NotImplementedError()
 
-    def validate_element(self, element, state, direction):
-        return containers.Mapping.validate_element(
-            self, element, state, direction)
+    def serialize(self, element, value):
+        raise TypeError("Not implemented for Compound types.")
+
 
 class DateYYYYMMDD(Compound, scalars.Date):
     def __init__(self, name, *specs, **kw):
@@ -78,6 +122,7 @@ class DateYYYYMMDD(Compound, scalars.Date):
             specs.append(scalars.Integer('day', format=u'%02i'))
 
         super(DateYYYYMMDD, self).__init__(name, *specs, **kw)
+        self.specs = specs
 
     def compose(self, element):
         try:
@@ -88,7 +133,7 @@ class DateYYYYMMDD(Compound, scalars.Date):
             value = scalars.Date.adapt(self, element, as_str)
             return as_str, value
         except (exc.AdaptationError, TypeError):
-            return None, None
+            return u'', None
 
     def explode(self, element, value):
         try:
---
+++ tests/schema/test_compound.py
@@ -0,0 +1,195 @@
+import datetime
+from nose.tools import eq_, assert_raises, set_trace, raises
+from flatland import schema, util
+
+
+class TestDoubleField(object):
+    def setup(self):
+        class Double(schema.Compound):
+            def __init__(self, name, **kw):
+                children = [schema.Integer('x'), schema.Integer('y')]
+                schema.Compound.__init__(self, name, *children, **kw)
+
+            def compose(self, element):
+                ex, ey = element.get('x'), element.get('y')
+                ux, uy = ex.u, ey.u
+                if ex.u and ey.u:
+                    string = "%sx%s" % (ex.u, ey.u)
+                else:
+                    string = ''
+
+                if ex.value is not None and ey.value is not None:
+                    value = (ex.value, ey.value)
+                else:
+                    value = None
+
+                return string, value
+
+            def explode(self, element, value):
+                try:
+                    x, y = value
+                except (TypeError, ValueError):
+                    return
+                element['x'].set(x)
+                element['y'].set(y)
+        self.Double = Double
+
+    def test_from_flat(self):
+        s = self.Double('s')
+
+        e = s.from_flat({})
+        assert_values_(e, None, None, None)
+        assert_us_(e, u'', u'', u'')
+
+        e = s.from_flat({u's': u'1x2'})
+        assert_values_(e, None, None, None)
+        assert_us_(e, u'', u'', u'')
+
+        e = s.from_flat({u's_x': u'1'})
+        assert_values_(e, None, 1, None)
+        assert_us_(e, u'', u'1', u'')
+
+        e = s.from_flat({u's_x': u'1', u's_y': u'2'})
+        assert_values_(e, (1, 2), 1, 2)
+        assert_us_(e, u'1x2', u'1', u'2')
+
+        e = s.from_flat({u's_x': u'1', u's_y': u'2', u's': u'5x5'})
+        assert_values_(e, (1, 2), 1, 2)
+        assert_us_(e, u'1x2', u'1', u'2')
+
+    def test_set(self):
+        s = self.Double('s')
+
+        e = s.create_element(value={})
+        assert_values_(e, None, None, None)
+        assert_us_(e, u'', u'', u'')
+
+        e = s.create_element(value=u'4x5')
+        assert_values_(e, None, None, None)
+        assert_us_(e, u'', u'', u'')
+
+        e = s.create_element(value=(4, 5))
+        assert_values_(e, (4, 5), 4, 5)
+        assert_us_(e, u'4x5', u'4', u'5')
+
+    def test_update(self):
+        s = self.Double('s')
+
+        e = s.create_element(value=(4, 5))
+        assert_values_(e, (4, 5), 4, 5)
+        assert_us_(e, u'4x5', u'4', u'5')
+
+        e['x'].set(6)
+        assert_values_(e, (6, 5), 6, 5)
+        assert_us_(e, u'6x5', u'6', u'5')
+
+        e.set((7,8))
+        assert_values_(e, (7, 8), 7, 8)
+        assert_us_(e, u'7x8', u'7', u'8')
+
+    def test_explode_does_not_require_typechecks(self):
+        s = self.Double('s')
+
+        e = s.create_element(value=(4, 5))
+        assert_values_(e, (4, 5), 4, 5)
+        assert_us_(e, u'4x5', u'4', u'5')
+
+        e.set((7, 'blagga'))
+        assert_values_(e, None, 7, None)
+        assert_us_(e, u'7xblagga', u'7', u'blagga')
+
+    def test_value_assignement(self):
+        s = self.Double('s')
+
+        e = s.create_element()
+        e.value = (1, 2)
+        assert_values_(e, (1, 2), 1, 2)
+        assert_us_(e, u'1x2', u'1', u'2')
+
+        e = s.create_element()
+        e.u = "1x2"
+        assert_values_(e, None, None, None)
+        assert_us_(e, u'', u'', u'')
+
+    def test_repr(self):
+        s = self.Double('s')
+        e = s.create_element()
+        assert repr(e)
+
+        e.set((1, 2))
+        assert repr(e)
+
+        e.set(None)
+        assert repr(e)
+
+def test_repr_always_safe():
+    # use the abstract class to emulate a subclass with broken compose.
+    broken_impl = schema.Compound('x', schema.String('y'))
+    e = broken_impl.create_element()
+    assert repr(e)
+
+@raises(NotImplementedError)
+def test_explode_abstract():
+    e = schema.Compound('x', schema.String('y')).create_element()
+    e.set('x')
+
+@raises(NotImplementedError)
+def test_compose_abstract():
+    e = schema.Compound('x', schema.String('y')).create_element()
+    e.value
+
+@raises(TypeError)
+def test_compose_abstract():
+    # really it'd be nice if serialize simply wasn't inherited. would
+    # have to rejigger the hierarchy, not sure its worth it.
+    s = schema.Compound('x', schema.String('y'))
+    e = s.create_element()
+    s.serialize(e, 'abc')
+
+def assert_values_(el, top, x, y):
+    eq_(el.value, top)
+    eq_(el['x'].value, x)
+    eq_(el['y'].value, y)
+
+def assert_us_(el, top, x, y):
+    eq_(el.u, top)
+    eq_(el['x'].u, x)
+    eq_(el['y'].u, y)
+
+def test_sample_compound():
+    s = schema.DateYYYYMMDD('s')
+    assert 'year' in s.fields
+    assert 'month' in s.fields
+    assert 'day' in s.fields
+
+    e = s.create_element()
+    assert e.value is None
+    assert e['year'].value is None
+    assert e.el('year').value is None
+
+    when = datetime.date(2000, 10, 1)
+    e.set(when)
+    assert e.value == when
+    assert e.el('year').value == 2000
+    assert e.el('day').value == 1
+    assert e.u == u'2000-10-01'
+    assert e.el('day').u == u'01'
+
+    e.el('day').set(5)
+    assert e.value == datetime.date(2000, 10, 5)
+    assert e.el('day').value == 5
+    assert e.u == u'2000-10-05'
+
+    e = s.create_element()
+    e.set(None)
+    assert e.value is None
+    assert e.u == u''
+    assert e['year'].value is None
+    assert e.el('year').value is None
+
+    e = s.create_element()
+    e.set('snaggle')
+    assert e.value is None
+    assert e.u == u''
+    assert e['year'].value is None
+    assert e.el('year').value is None
