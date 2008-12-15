ce979a15134d4ddf093bbc085be45af828bd7a30
jason kirtland <jek@discorporate.us>
2008-12-14 16:34:09 -0800

    A couple small efficiency tweaks.

--- flatland/util/signals.py
+++ flatland/util/signals.py
@@ -110,10 +110,11 @@ class Signal(object):
         value. The ordering of receiver notification is undefined.
 
         """
-        results = []
-        for receiver in self.receivers_for(sender):
-            results.append((receiver, receiver(sender=sender, **kwargs)))
-        return results
+        if not self._receivers:
+            return []
+        else:
+            return [(receiver, receiver(sender=sender, **kwargs))
+                    for receiver in self.receivers_for(sender)]
 
     def has_receivers_for(self, sender):
         """True if there is probably a receiver for *sender*.
@@ -134,23 +135,23 @@ class Signal(object):
     def receivers_for(self, sender):
         """Iterate all live receivers listening for *sender*."""
         if self.has_connected:
-            seen = set()
-            for bucket in ANY_ID, _hashable_identity(sender):
-                for receiver_id in list(self._by_sender[bucket]):
-                    receiver = self._receivers.get(receiver_id)
-                    if receiver is None:
+            sender_id = _hashable_identity(sender)
+            if sender_id in self._by_sender:
+                ids = (self._by_sender[ANY_ID] |
+                       self._by_sender[sender_id])
+            else:
+                ids = self._by_sender[ANY_ID].copy()
+            for receiver_id in ids:
+                receiver = self._receivers.get(receiver_id)
+                if receiver is None:
+                    continue
+                if isinstance(receiver, weakrefs.WeakTypes):
+                    strong = receiver()
+                    if strong is None:
+                        self._disconnect(receiver_id, ANY_ID)
                         continue
-                    if isinstance(receiver, weakrefs.WeakTypes):
-                        strong = receiver()
-                        if strong is None:
-                            self._disconnect(receiver_id, ANY_ID)
-                            continue
-                        receiver = strong
-                    if bucket == ANY_ID:
-                        seen.add(id(receiver))
-                        yield receiver
-                    elif id(receiver) not in seen:
-                        yield receiver
+                    receiver = strong
+                yield receiver
 
     def disconnect(self, receiver, sender=ANY):
         """Disconnect *receiver* from this signal's events."""
