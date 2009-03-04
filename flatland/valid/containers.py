d876742cebc149c565ce582038142dfa06cc77b9
jason kirtland <jek@discorporate.us>
2009-03-04 11:39:39 -0800

    NotDuplicated comparator is now pluggable.

--- flatland/valid/containers.py
+++ flatland/valid/containers.py
@@ -1,3 +1,4 @@
+import operator
 from ..schema import Slot
 from . base import Validator
 
@@ -18,6 +19,48 @@ class NotDuplicated(Validator):
         String('favorite_color', validators=[
           NotDuplicated(failure="Please enter each color only once.")]))
 
+    **Attributes**
+
+    .. attribute:: comparator
+
+      A callable boolean predicate, by default ``operator.eq``.
+      Called positionally with two arguments, *element* and *sibling*.
+
+      Can be used as a filter, for example ignoring any siblings that
+      have been marked as "deleted" by a checkbox in a web form:
+
+      .. testcode::
+
+        from flatland import Dict, List, String, Integer, Boolean
+        from flatland.valid import NotDuplicated
+
+        def live_addrs(element, sibling):
+            thisval, thatval = element.value, sibling.value
+            # data marked as deleted is never considered a dupe
+            if thisval['deleted'] or thatval['deleted']:
+                return False
+            # compare elements on 'street' & 'city', ignoring 'id'
+            return (thisval['street'] == thatval['street'] and
+                    thisval['city'] == thatval['city'])
+
+        schema = List('addresses',
+                      Dict('address',
+                           Integer('id', optional=True),
+                           Boolean('deleted'),
+                           String('street'),
+                           String('city'),
+                           validators=[NotDuplicated(comparator=live_addrs)]
+                           ))
+
+    .. testcode:: :hide:
+
+        data = {'id': 1, 'deleted': False, 'street': 'a', 'city': 'b'}
+        el = schema.create_element(value=[data, data])
+        assert not el.validate()
+        del el[:]
+        el.set([data, dict(data, deleted=True)])
+        assert el.validate()
+
     **Messages**
 
     .. attribute:: failure
@@ -31,6 +74,8 @@ class NotDuplicated(Validator):
 
     failure = u'%(label)s may not be repeated within %(container_label)s.'
 
+    comparator = operator.eq
+
     def validate(self, element, state):
         if element.parent is None:
             raise TypeError(
@@ -42,11 +87,12 @@ class NotDuplicated(Validator):
         if isinstance(container, Slot):
             container = container.parent
         valid, position = True, 0
+        op = self.comparator
         for idx, sibling in enumerate(container.children):
             if sibling is element:
                 position = idx + 1
                 break
-            if valid and element == sibling:
+            if valid and op(element, sibling):
                 valid = False
         if not valid:
             return self.note_error(
