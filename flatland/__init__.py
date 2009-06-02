8b123fdfc0ecaf8a32bd3a5060c9a7fe4c59f71c
jason kirtland <jek@discorporate.us>
2009-06-02 11:21:36 -0700

    Armined the place up a bit with deferred push-up imports in __init__

--- flatland/__init__.py
+++ flatland/__init__.py
@@ -1,35 +1,73 @@
-import exc
-import util
-import signals
-import schema
-from schema import (
-    Array,
-    Boolean,
-    Compound,
-    Date,
-    DateTime,
-    DateYYYYMMDD,
-    Dict,
-    Enum,
-    FieldSchema,
-    Float,
-    Form,
-    Integer,
-    List,
-    Long,
-    MultiValue,
-    Ref,
-    Skip,
-    SkipAll,
-    SkipAllFalse,
-    String,
-    Time,
-    )
-import valid
+import sys
+import types
 
 
-__version__ = '0.0.2'
-__version_info__ = (0, 0, 2)
+class deferred_module(types.ModuleType):
+    """A module whose __all__ members are loaded on first access."""
 
-__author__ = 'jason kirtland <jek@discorporate.us>'
+    @classmethod
+    def shadow(cls, module_name, deferred, **attributes):
+        """Replace *module_name* in ``sys.modules`` with a deferred clone."""
+        module = sys.modules[module_name]
+        sys.modules[module_name] = cls(module, deferred, **attributes)
+
+    def __init__(self, module, deferred, **attributes):
+        types.ModuleType.__init__(self, module.__name__, module.__doc__ or None)
+        self.__dict__.update(attributes)
+        self.__shadowing = module
+        self.__all__ = []
+        self.__pushed_up = {}
+        self.__file__ = module.__file__
+        self.__path__ = module.__path__
+
+        for submodule, pushed_up in deferred.iteritems():
+            self.__all__.append(submodule)
+            if pushed_up:
+                for member in pushed_up:
+                    self.__pushed_up[member] = submodule
+                self.__all__.extend(pushed_up)
+
+    def __getattr__(self, key):
+        if key in self.__pushed_up:
+            owner = self.__pushed_up[key]
+            value = getattr(getattr(self, owner), key)
+            setattr(self, key, value)
+            return value
+        elif key in self.__all__:
+            module = __import__(
+                self.__name__ + '.' + key, None, None, [self.__name__])
+            setattr(self, key, module)
+            return module
+        else:
+            return types.ModuleType.__getattribute__(self, key)
+
+
+deferred_module.shadow('flatland',
+                       { 'exc': (),
+                         'schema': ('Array',
+                                    'Boolean',
+                                    'Compound',
+                                    'Date',
+                                    'DateTime',
+                                    'DateYYYYMMDD',
+                                    'Dict',
+                                    'Enum',
+                                    'FieldSchema',
+                                    'Float',
+                                    'Form',
+                                    'Integer',
+                                    'List',
+                                    'Long',
+                                    'MultiValue',
+                                    'Ref',
+                                    'Skip',
+                                    'SkipAll',
+                                    'SkipAllFalse',
+                                    'String',
+                                    'Time',
+                                    ),
+                         'signals': (),
+                         'util': (),
+                         'valid': (),
+                         })
 
