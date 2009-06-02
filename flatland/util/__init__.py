1cea66a8e87cc04de8f170ba4e02e0ed2dc0bb6a
jason kirtland <jek@discorporate.us>
2009-06-02 11:21:36 -0700

    Armined the place up a bit with deferred push-up imports in __init__

--- flatland/util/__init__.py
+++ flatland/util/__init__.py
@@ -1,22 +1,29 @@
 """Utilities."""
-from . base import (
-    Maybe,
-    Unspecified,
-    adict,
-    as_mapping,
-    assignable_property,
-    decorator,
-    format_argspec_plus,
-    keyslice_pairs,
-    lazy_property,
-    luhn10,
-    named_int_factory,
-    re_ucompile,
-    symbol,
-    threading,
-    to_pairs,
-    )
-from . signals import (
-    Signal,
-    signal
-    )
+import sys
+
+
+type(sys.modules['flatland']).shadow(
+    'flatland.util',
+    {
+        'base': (
+            'Maybe',
+            'Unspecified',
+            'adict',
+            'as_mapping',
+            'assignable_property',
+            'decorator',
+            'format_argspec_plus',
+            'keyslice_pairs',
+            'lazy_property',
+            'luhn10',
+            'named_int_factory',
+            're_ucompile',
+            'symbol',
+            'threading',
+            'to_pairs',
+            ),
+        'signals': (
+            'Signal',
+            'signal',
+            ),
+      })
