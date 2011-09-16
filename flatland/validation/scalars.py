2c0bef9de3d13a32963ccb94046fa2bb9ddd2872
Dag Odenhall <dag.odenhall@gmail.com>
2011-09-16 23:18:41 +0200

    Fix failing doctest since validation docs split up

--- flatland/validation/scalars.py
+++ flatland/validation/scalars.py
@@ -582,10 +582,10 @@ class ValuesEqual(MapEqual):
 
     .. testcode::
 
-      import flatland
+      from flatland import Form, String
       from flatland.validation import ValuesEqual
 
-      class MyForm(flatland.Form):
+      class MyForm(Form):
           password = String
           password_again = String
           validators = [ValuesEqual('password', 'password_again')]
