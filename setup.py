c37d183187a61736e28dadaf56c2f1386615f494
Thomas Waldmann <tw@waldmann-edv.de>
2018-10-31 00:49:57 +0100

    drop support for python 2.6, fixes #16

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 import os
 import sys
 
-if sys.version_info < (2, 6):
-    print "Python 2.6 or higher is required."
+if sys.version_info < (2, 7):
+    print "Python 2.7.x is required."
     sys.exit(1)
 
 try:
@@ -43,7 +43,6 @@ setup(name="flatland",
                    'Natural Language :: English',
                    'Operating System :: OS Independent',
                    'Programming Language :: Python',
-                   'Programming Language :: Python :: 2.6',
                    'Programming Language :: Python :: 2.7',
                    'Topic :: Internet :: WWW/HTTP :: WSGI',
                    'Topic :: Software Development :: Libraries'],
